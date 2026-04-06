9f1044f9986544e5af2faaee12850ac81a634c62
msayedq <msayedqazizadah590@gmail.com>
2026-04-06 12:23:42 +0430

    svelte apps ready

--- apps/svelte_app/vite.config.ts
+++ apps/svelte_app/vite.config.ts
@@ -1,8 +1,12 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      preprocess: vitePreprocess(),
+    }),
+  ],
   server: { port: 5180 },
   preview: { port: 5180 },
   envPrefix: 'VITE_',
